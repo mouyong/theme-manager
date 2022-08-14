0042fca0995decc600e7bf16cc4cdd025bd75e93
mouyong <my24251325@gmail.com>
2022-08-14 18:07:51 +0800

    feat: Init.

---
+++ config/themes.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jarvis Tang
+ * Released under the Apache-2.0 License.
+ */
+
+return [
+    'namespace' => $themesNamespace = 'Themes',
+
+    // YOU COULD CUSTOM HERE
+    'namespaces' => [
+        $themesNamespace => [
+            base_path('extensions/themes'),
+        ],
+    ],
+
+    'autoload_files' => [
+        // base_path('demo.php'),
+    ],
+
+    'merge_plugin_config' => [
+        'include' => [
+            ltrim(str_replace(base_path(), '', base_path('extensions/themes/*/composer.json')), '/'),
+        ],
+        'recurse' => true,
+        'replace' => false,
+        'ignore-duplicates' => false,
+        'merge-dev' => true,
+        'merge-extra' => true,
+        'merge-extra-deep' => true,
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Composer File Template
+    |--------------------------------------------------------------------------
+    |
+    | YOU COULD CUSTOM HERE
+    |
+    */
+    'composer'  => [
+        'vendor' => 'fresns',
+        'author' => [
+            [
+                'name'  => 'Jarvis Tang',
+                'email' => 'jarvis.okay@gmail.com',
+                'homepage' => 'https://github.com/jarvis-tang',
+                'role' => 'Creator',
+            ],
+            [
+                'name'  => 'mouyong',
+                'email' => 'my24251325@gmail.com',
+                'homepage' => 'https://github.com/mouyong',
+                'role' => 'Developer',
+            ],
+        ],
+    ],
+
+    'paths' => [
+        'base' => base_path('extensions'),
+        'backups' => base_path('extensions/backups/themes'),
+        'themes' => base_path('extensions/themes'),
+        'assets' => public_path('assets/themes'),
+
+        'generator' => [
+            'provider'          => ['path' => 'app/Providers', 'generate' => true],
+            'assets'            => ['path' => 'assets', 'generate' => true],
+            'lang'              => ['path' => 'lang', 'generate' => true],
+            'views'             => ['path' => 'views', 'generate' => true],
+        ],
+    ],
+
+    'stubs' => [
+        'path'         => dirname(__DIR__).'/src/Commands/stubs',
+        'files'        => [
+            'scaffold/provider'     => 'app/Providers/$STUDLY_NAME$ServiceProvider.php',
+            'assets/js/app'         => 'assets/js/app.js',
+            'assets/sass/app'       => 'assets/sass/app.scss',
+            'composer.json'         => 'composer.json',
+            'theme.json'            => 'theme.json',
+            'readme'                => 'README.md',
+        ],
+        'gitkeep'      => true,
+    ],
+
+    'manager' => [
+        'default' => [
+            'file' => base_path('fresns.json'),
+        ],
+    ],
+];
---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * Fresns (https://fresns.org)
+ * Copyright (C) 2021-Present Jarvis Tang
+ * Released under the Apache-2.0 License.
+ */
+
+namespace Fresns\ThemeManager\Providers;
+
+use Fresns\ThemeManager\Theme;
+use Fresns\PluginManager\Support\Json;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->autoload();
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/themes.php', 'themes');
+        $this->publishes([
+            __DIR__.'/../../config/themes.php' => config_path('themes.php'),
+        ], 'laravel-theme-config');
+
+        $this->addMergePluginConfig();
+
+        $this->registerCommands([
+            __DIR__.'/../Commands/*',
+        ]);
+    }
+
+    public function registerCommands($paths)
+    {
+        $allCommand = [];
+
+        foreach ($paths as $path) {
+            $commandPaths = glob($path);
+
+            foreach ($commandPaths as $command) {
+                $commandPath = realpath($command);
+                if (! is_file($commandPath)) {
+                    continue;
+                }
+
+                $commandClass = 'Fresns\\ThemeManager\\Commands\\'.pathinfo($commandPath, PATHINFO_FILENAME);
+
+                if (class_exists($commandClass)) {
+                    $allCommand[] = $commandClass;
+                }
+            }
+        }
+
+        $this->commands($allCommand);
+    }
+
+    protected function autoload()
+    {
+        $this->addFiles();
+    }
+
+    protected function addFiles()
+    {
+        $files = $this->app['config']->get('themes.autoload_files');
+
+        foreach ($files as $file) {
+            if (file_exists($file)) {
+                require_once $file;
+            }
+        }
+    }
+
+    protected function addMergePluginConfig()
+    {
+        $composerPath = base_path('composer.json');
+        $composer = Json::make($composerPath)->get();
+
+        $userMergePluginConfig = Arr::get($composer, 'extra.merge-plugin', []);
+
+        $defaultMergePlugin = config('themes.merge_plugin_config');
+
+        $mergePluginConfig = array_merge($defaultMergePlugin, $userMergePluginConfig);
+
+        // merge include
+        $diffInclude = array_diff($defaultMergePlugin['include'], $userMergePluginConfig['include']);
+        $mergePluginConfigInclude = array_merge($diffInclude, $userMergePluginConfig['include']);
+
+        $mergePluginConfig['include'] = $mergePluginConfigInclude;
+
+        Arr::set($composer, 'extra.merge-plugin', $mergePluginConfig);
+
+        try {
+            $content = Json::make()->encode($composer);
+
+            file_put_contents($composerPath, $content);
+        } catch (\Throwable $e) {
+            $message = str_replace(['file_put_contents('.base_path().'/', ')'], '', $e->getMessage());
+            throw new \RuntimeException('cannot set merge-plugin to '.$message);
+        }
+    }
+}
