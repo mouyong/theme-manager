e9cbc86208c7db5439eb6317144952454e9d1e7d
Jarvis <jarvis.okay@gmail.com>
2022-08-27 17:51:19 +0800

    docs: 修改文案

--- src/Commands/ThemeInstallCommand.php
+++ src/Commands/ThemeInstallCommand.php
@@ -26,7 +26,7 @@ class ThemeInstallCommand extends Command
         try {
             $path = $this->argument('path');
             if (str_contains($path, config('themes.paths.plugins'))) {
-                $this->error('Failed to install themes from extensions/themes directory');
+                $this->error('Failed to install themes from theme directory');
                 return 0;
             }
 
